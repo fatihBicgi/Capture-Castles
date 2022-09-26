6c98ed3093d00aee7087380751632f2853f7d64b
Fatih Bicgi <99922562+fatihBicgi@users.noreply.github.com>
2022-09-27 00:50:34 +0300

    airforce enhancer trigger added

--- Assets/Scripts/Enhancer.cs
+++ Assets/Scripts/Enhancer.cs
@@ -4,43 +4,105 @@ using UnityEngine;
 
 public class Enhancer : MonoBehaviour
 {
-    private GameObject infantryLeftSpawn, infantryRightSpawn, leftLineParent, rightLineParent;
+    private GameObject infantryLeftSpawn, infantryRightSpawn, infantryLeftLineParent, infantryRightLineParent,
+         airforceLeftSpawn, airforceRightSpawn,airforceLeftLineParent, airforceRightLineParent;
 
     [SerializeField]
     GameObject infantryPrefab, dragonPrefab;
+
     private void Start()
     {
-        infantryLeftSpawn = GameObject.Find("Infantry Left Origin Spawn Point (1)");
-        infantryRightSpawn = GameObject.Find("Infantry Right Origin Spawn Point (1)");
+        InfantryAssignment();
 
-        leftLineParent = GameObject.Find("LeftLine");
-        rightLineParent = GameObject.Find("RightLine");
+        AirforceAssignment();
+
     }
-
+  
     private void OnTriggerEnter(Collider other)
-    {
-        
+    {      
         if (other.tag == "Infantry")
         {
             FindObjectOfType<ArmyCount>().currentInfantryCount++;
 
-            if (FindObjectOfType<ArmyCount>().currentInfantryCount % 2 == 0 && !(FindObjectOfType<ArmyCount>().currentInfantryCount == 0))
+            bool isCurrentInfantryCountOdd = FindObjectOfType<ArmyCount>().currentInfantryCount % 2 == 1;
+
+
+            if ( !(isCurrentInfantryCountOdd) && FindObjectOfType<ArmyCount>().currentInfantryCount > 0)
             {
-                GameObject clone = Instantiate(infantryPrefab, infantryLeftSpawn.transform.position, infantryLeftSpawn.transform.rotation);
-                clone.transform.parent = leftLineParent.transform;
-                infantryLeftSpawn.transform.position += infantryLeftSpawn.transform.TransformDirection(Vector3.fwd);
+                LeftInstantiateNewInfantry();
+
+                LeftSetNewInfantrySpawnPoint();
             }
-            if (FindObjectOfType<ArmyCount>().currentInfantryCount % 2 == 1)
+            if ( isCurrentInfantryCountOdd)
             {
-                GameObject clone = Instantiate(infantryPrefab, infantryRightSpawn.transform.position, infantryRightSpawn.transform.rotation);
-                clone.transform.parent = rightLineParent.transform;
-                infantryRightSpawn.transform.position += infantryRightSpawn.transform.TransformDirection(Vector3.fwd);
+                RightInstantiateNewInfantry();
+
+                RightSetNewInfantrySpawnPoint();
             }
         }
         else if (other.tag == "Dragon")
         {
             FindObjectOfType<ArmyCount>().currentDragonCount++;
+
+            bool isCurrentDragonCountOdd = FindObjectOfType<ArmyCount>().currentDragonCount % 2 == 1;
+
+            if (!(isCurrentDragonCountOdd))
+            {
+                airforceLeftSpawn.transform.position += airforceLeftSpawn.transform.TransformDirection(Vector3.up);
+
+                GameObject clone = Instantiate(dragonPrefab, airforceLeftSpawn.transform.position, airforceLeftSpawn.transform.rotation);
+                clone.transform.parent = airforceLeftLineParent.transform;
+
+                
+            }
+            if (isCurrentDragonCountOdd)
+            {
+                GameObject clone = Instantiate(dragonPrefab, airforceRightSpawn.transform.position, airforceRightSpawn.transform.rotation);
+                clone.transform.parent = airforceRightLineParent.transform;
+
+                airforceRightSpawn.transform.position += airforceRightSpawn.transform.TransformDirection(Vector3.up);
+            }
+
         }
     }
-   
+    private void AirforceAssignment()
+    {
+        airforceLeftSpawn = GameObject.Find("Airforce Left Origin Spawn Point");
+        airforceRightSpawn = GameObject.Find("Airforce Right Origin Spawn Point");
+
+        airforceLeftLineParent = GameObject.Find("AirForce Left Line");
+        airforceRightLineParent = GameObject.Find("AirForce Right Line");
+    }
+
+    private void InfantryAssignment()
+    {
+        infantryLeftSpawn = GameObject.Find("Infantry Left Origin Spawn Point");
+        infantryRightSpawn = GameObject.Find("Infantry Right Origin Spawn Point");
+
+        infantryLeftLineParent = GameObject.Find("Infantry Left Line");
+        infantryRightLineParent = GameObject.Find("Infantry Right Line");
+    }
+
+
+    private void RightInstantiateNewInfantry()
+    {
+        GameObject clone = Instantiate(infantryPrefab, infantryRightSpawn.transform.position, infantryRightSpawn.transform.rotation);
+        clone.transform.parent = infantryRightLineParent.transform;
+    }
+
+    private void LeftInstantiateNewInfantry()
+    {
+        GameObject clone = Instantiate(infantryPrefab, infantryLeftSpawn.transform.position, infantryLeftSpawn.transform.rotation);
+        clone.transform.parent = infantryLeftLineParent.transform;
+    }
+
+    private void RightSetNewInfantrySpawnPoint()
+    {
+        infantryRightSpawn.transform.position += infantryRightSpawn.transform.TransformDirection(Vector3.forward);
+    }
+
+    private void LeftSetNewInfantrySpawnPoint()
+    {
+        infantryLeftSpawn.transform.position += infantryLeftSpawn.transform.TransformDirection(Vector3.forward);
+    }
 }
